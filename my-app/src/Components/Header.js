6e1c75c997bda5babfb42e58f2046df941552475
Dhileepguru <dhileepguru497@gmail.com>
2024-08-17 08:33:57 +0530

    Created Components Folder

---
+++ my-app/src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../Assets/images/spotknackLogo.png';
+function Header()
+{
+    return(
+        <section>
+           <img src={logo}></img>
+        </section>
+    )
+}
+
+
+
+
+export default Header;
